4c0f3cf55e4d3e995d84dca72a41c519523c4369
Jalon Wong <jalonwong@gmail.com>
2025-06-13 21:07:12 -0500

    Format scripts

--- toolchain/arm_clang_version.py
+++ toolchain/arm_clang_version.py
@@ -1,9 +1,9 @@
-import re
 import os
-import sys
+import re
 import subprocess
+import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) >= 2:
         arm_path = sys.argv[1]
         if not os.path.exists(arm_path):
@@ -11,12 +11,12 @@ if __name__ == '__main__':
     else:
         exit(1)
 
-    arm_bin = os.path.join(arm_path, 'bin', 'armclang')
-    ret = subprocess.run([arm_bin, '--version'], capture_output=True, text=True)
+    arm_bin = os.path.join(arm_path, "bin", "armclang")
+    ret = subprocess.run([arm_bin, "--version"], capture_output=True, text=True)
     if ret.returncode != 0:
         exit(ret.returncode)
 
-    obj = re.search('A[rR][mM] Compiler\D+([\d\.]+)', ret.stdout)
+    obj = re.search(r"A[rR][mM] Compiler\D+([\d\.]+)", ret.stdout)
     if obj:
         print(obj.group(1))
         exit(0)
